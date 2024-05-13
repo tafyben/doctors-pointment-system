9bfe0561ef9777795bf77905a3b4491ccb83b25b
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-05-13 15:04:03 +0200

    added necessary comments for the register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'gender' => ['required', 'string'],
         ]);
     }
 
@@ -64,6 +65,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // state the role
         $role = Role::where('name', 'patient')->first();
         return User::create([
             'name' => $data['name'],
