da4973403e9b7e57fa3f58c25bf946f76305fcad
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-06-24 18:18:01 +0200

    refactored error showing on the project

--- resources/views/admin/doctor/create.blade.php
+++ resources/views/admin/doctor/create.blade.php
@@ -31,23 +31,25 @@
                                         <div class="form-group">
                                             <label>Full Name:</label>
                                             <input type="text" class="form-control @error('name') is-invalid @enderror" name="name">
-                                        </div>
-                                        @error('name')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('name')
+                                            <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Email:</label>
                                             <input type="email" class="form-control @error('email') is-invalid @enderror" name="email">
-                                        </div>
-                                        @error('email')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div class="row">
@@ -55,23 +57,25 @@
                                         <div class="form-group">
                                             <label>Password:</label>
                                             <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
-                                        </div>
-                                        @error('password')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Phone:</label>
                                             <input type="text" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number">
-                                        </div>
-                                        @error('phone_number')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('phone_number')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div class="row">
@@ -79,56 +83,60 @@
                                         <div class="form-group">
                                             <label>Address line:</label>
                                             <input type="text" class="form-control @error('address') is-invalid @enderror" name="address">
-                                        </div>
-                                        @error('address')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('address')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Role:</label>
-                                            <select class="select @error('role_id') is-invalid @enderror" name="role_id">
-                                                <option>Select Role</option>
+                                            <select class="select form-control @error('role_id') is-invalid @enderror" name="role_id">
+                                                <option value="">Select Role</option>
                                                 @foreach(App\Models\Role::where('name','!=','patient')->get() as $role)
                                                     <option value="{{$role->id}}">{{$role->name}}</option>
                                                 @endforeach
                                             </select>
-                                        </div>
-                                        @error('role_id')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('role_id')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Gender:</label>
-                                            <select class="select @error('gender') is-invalid @enderror" name="gender">
-                                                <option>Select Gender</option>
+                                            <select class="select form-control @error('gender') is-invalid @enderror" name="gender">
+                                                <option value="">Select Gender</option>
                                                 <option value="1">Male</option>
                                                 <option value="2">Female</option>
                                             </select>
-                                        </div>
-                                        @error('gender')
-                                        <span class="invalid-feedback" role="alert">
+                                            @error('gender')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Specialist:</label>
-                                            <input type="text" class="form-control @error('specialist') is-invalid @enderror" name="specialist">
-                                        </div>
-                                        @error('specialist')
-                                        <span class="invalid-feedback" role="alert">
+                                            <input type="text" class="form-control @error('department') is-invalid @enderror" name="department">
+                                            @error('department')
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                        @enderror
+                                            @enderror
+                                        </div>
+
                                     </div>
                                 </div>
                                 <div class="row">
@@ -136,34 +144,37 @@
                                         <div class="form-group">
                                             <label>Education:</label>
                                             <input type="text" class="form-control @error('education') is-invalid @enderror" name="education">
+                                            @error('education')
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                            @enderror
                                         </div>
-                                        @error('education')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
+
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label>Image:</label>
                                             <input type="file" class="form-control @error('image') is-invalid @enderror" name="image">
+                                            @error('image')
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                            @enderror
                                         </div>
-                                        @error('image')
-                                        <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                        @enderror
+
                                     </div>
                                     <div class="col-md-12">
                                     <div class="form-group">
-                                        <label>Descrition:</label>
-                                        <textarea rows="5" cols="5" class="form-control" placeholder="Enter message" name="description"></textarea>
-                                    </div>
+                                        <label>Description:</label>
+                                        <textarea rows="5" cols="5" class="form-control @error('description') is-invalid @enderror"   placeholder="Enter message" name="description"></textarea>
                                         @error('description')
                                         <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                            <strong>{{ $message }}</strong>
+                                        </span>
                                         @enderror
+                                    </div>
+
                                     </div>
                         </div>
                             </div>
