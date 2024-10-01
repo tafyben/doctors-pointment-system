1ae9f2378d6ccd090b0dad6beb8ba30db5d8be30
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-10-01 18:54:52 +0200

    create the appointment foler - incuded the create blade

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -11,7 +11,7 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
---
+++ resources/views/admin/appointment/create.blade.php
@@ -0,0 +1,17 @@
+@extends('admin.layouts.app')
+
+@section('content')
+    <!-- Page Header -->
+    <div class="page-header">
+        <div class="row">
+            <div class="col">
+                <h3 class="page-title">Doctors</h3>
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                    <li class="breadcrumb-item active">Create</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+@endsection
