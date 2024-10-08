e91a2ba683ef1afea8c010680f8bd8ce8e758fa3
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-10-08 16:32:17 +0200

    working on frontend controller

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+
+Route::get('/', [\App\Http\Controllers\FrontendController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
