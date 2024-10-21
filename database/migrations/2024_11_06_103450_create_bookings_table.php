5dcb23d872f4513fdafc814f0848ab2be3016be9
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-10-21 13:02:01 +0200

    Mde booking column and added the values

---
+++ database/migrations/2024_11_06_103450_create_bookings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('doctor_id');
+            $table->string('time');
+            $table->integer('status')->default(0);
+            $table->string('date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
