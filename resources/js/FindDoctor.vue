dd28cbeabb1c7822237d1ebcccee409c69968031
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-11-09 10:59:29 +0200

    installed vue loader and showed it to the frontend

--- resources/js/FindDoctor.vue
+++ resources/js/FindDoctor.vue
@@ -38,12 +38,15 @@
                             </a>
                         </td>
                     </tr>
-                    <p>No doctors today</p>
+                    <td v-if="doctors.length==0">No doctors available for: <p class="bold">{{this.time}}</p> </td>
 
 
 
                     </tbody>
                 </table>
+                <div class="text-center">
+                    <beat-loader :loading="loading" :color="color" :size="size"></beat-loader>
+                </div>
 
 
             </div>
@@ -58,29 +61,53 @@
 <script type="text/javascript">
 import datepicker from 'vuejs-datepicker'
 import axios from 'axios';
-import moment from 'moment'
+import moment from 'moment';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import BeatLoader from 'vue-spinner/src/BeatLoader.vue';
     export default {
         data(){
             return{
                 time:'',
-                doctors:[]
+                doctors:[],
+                loading:false
             }
         },
         components:{
-            datepicker
+            datepicker,
+            PulseLoader,
+            BeatLoader
         },
         methods:{
             customDate(date){
+                //make loader appear
+                this.loading = true
                 this.time = moment(date).format('YYYY-MM-DD');
                 axios.post('/api/finddoctors',{date:this.time}).then((response)=>{
-                    this.doctors = response.data
+                    setTimeout(()=>{
+                        this.doctors = response.data
+                        this.loading = false
+                    }, 500)
                 }).catch((error)=>{alert('error')})
             }
         },
         mounted(){
+            this.loading = true
             axios.get('/api/doctors/today').then((response)=>{
                 this.doctors = response.data
+                this.loading = false
             })
         }
     }
 </script>
+
+<style scoped>
+
+/*
+style the date picker to take full space.
+.my-datepicker .my-datepicker_calendar{
+    width: 100%;
+    height: 330px;
+    font-weight: bold;
+}*/
+
+</style>
