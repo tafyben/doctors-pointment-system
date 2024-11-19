242ca6a327df7a8b96c57c131ba3ff538542c770
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-11-19 15:59:07 +0200

    show appointments made - in the admin backend using date

--- resources/views/admin/patientlist/index.blade.php
+++ resources/views/admin/patientlist/index.blade.php
@@ -3,9 +3,26 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">Appointments({{$bookings->count()}})</div>
+                    <form action="{{route('patient')}}" method="GET">
+
+                        <div class="card-header">
+                            Filter:
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control datetimepicker-input" id="main-datetime" data-toggle="datetimepicker" data-target="#main-datetime" name="date">
+                                </div>
+                                <div class="col-md-2">
+                                    <button type="submit" class="btn btn-primary">Search</button>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </form>
+
 
                     <div class="card-body">
                         <table class="table table-striped">
@@ -14,6 +31,8 @@
                                 <th scope="col">#</th>
                                 <th scope="col">Date</th>
                                 <th scope="col">User</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Gender</th>
                                 <th scope="col">Time</th>
                                 <th scope="col">Doctor</th>
                                 <th scope="col">Status</th>
@@ -25,6 +44,8 @@
                                     <th scope="row">{{$key+1}}</th>
                                     <td>{{$booking->date}}</td>
                                     <td>{{$booking->user->name}}</td>
+                                    <td>{{$booking->user->email}}</td>
+                                    <td>{{$booking->user->gender}}</td>
                                     <td>{{$booking->time}}</td>
                                     <td>{{$booking->doctor->name}}</td>
                                     <td>
@@ -47,3 +68,15 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script type="text/javascript">
+        $(function () {
+            $('#main-datetime').datetimepicker({
+                format: 'YYYY-MM-DD',
+                minDate: moment()
+
+            });
+        });
+    </script>
+@endpush
