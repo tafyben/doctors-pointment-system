5007ccf5d22c447d5d90797c87fd22e4400649a3
Tafadzwa Ben <tbmukwepa@gmail.com>
2024-11-21 12:08:35 +0200

    refactored admin dashboard

--- resources/views/admin/layouts/statistics.blade.php
+++ resources/views/admin/layouts/statistics.blade.php
@@ -4,14 +4,14 @@
             <div class="card-body">
                 <div class="dash-widget-header">
                                             <span class="dash-widget-icon text-primary border-primary">
-                                                <i class="fe fe-users"></i>
+                                                <i class="fe fe-heart-o"></i>
                                             </span>
                     <div class="dash-count">
-                        <h3>168</h3>
+                        <h3>{{App\Models\User::where('role_id', 3)->count()}}</h3>
                     </div>
                 </div>
                 <div class="dash-widget-info">
-                    <h6 class="text-muted">Doctors</h6>
+                    <h6 class="text-muted">Patients</h6>
                     <div class="progress progress-sm">
                         <div class="progress-bar bg-primary w-50"></div>
                     </div>
@@ -24,15 +24,16 @@
             <div class="card-body">
                 <div class="dash-widget-header">
                                             <span class="dash-widget-icon text-success">
-                                                <i class="fe fe-credit-card"></i>
+                                                <i class="fe fe-sunrise"></i>
+                                                <i class="fe fe-sunrise"></i>
                                             </span>
                     <div class="dash-count">
-                        <h3>487</h3>
+                        <h3>{{App\Models\User::where('role_id', 1)->count()}}</h3>
                     </div>
                 </div>
                 <div class="dash-widget-info">
 
-                    <h6 class="text-muted">Patients</h6>
+                    <h6 class="text-muted">Doctors</h6>
                     <div class="progress progress-sm">
                         <div class="progress-bar bg-success w-50"></div>
                     </div>
@@ -45,15 +46,15 @@
             <div class="card-body">
                 <div class="dash-widget-header">
                                             <span class="dash-widget-icon text-danger border-danger">
-                                                <i class="fe fe-money"></i>
+                                                <i class="fe fe-lock"></i>
                                             </span>
                     <div class="dash-count">
-                        <h3>485</h3>
+                        <h3>{{App\Models\Role::count()}}</h3>
                     </div>
                 </div>
                 <div class="dash-widget-info">
 
-                    <h6 class="text-muted">Appointment</h6>
+                    <h6 class="text-muted">Roles</h6>
                     <div class="progress progress-sm">
                         <div class="progress-bar bg-danger w-50"></div>
                     </div>
@@ -69,12 +70,34 @@
                                                 <i class="fe fe-folder"></i>
                                             </span>
                     <div class="dash-count">
-                        <h3>$62523</h3>
+                        <h3>{{App\Models\Booking::count()}}</h3>
                     </div>
                 </div>
                 <div class="dash-widget-info">
 
-                    <h6 class="text-muted">Specialities</h6>
+                    <h6 class="text-muted">Bookings</h6>
+                    <div class="progress progress-sm">
+                        <div class="progress-bar bg-warning w-50"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-xl-3 col-sm-6 col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="dash-widget-header">
+                                            <span class="dash-widget-icon text-warning border-warning">
+                                                <i class="fe fe-pencil"></i>
+                                            </span>
+                    <div class="dash-count">
+                        <h3>{{App\Models\Prescription::count()}}</h3>
+                    </div>
+                </div>
+                <div class="dash-widget-info">
+
+                    <h6 class="text-muted">Prescriptions</h6>
                     <div class="progress progress-sm">
                         <div class="progress-bar bg-warning w-50"></div>
                     </div>
